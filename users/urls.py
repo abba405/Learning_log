59a8278e55ac9536d00f7b6a4c2fda481e7f7b96
Abba <zakariyaibrahim3551@gmail.com>
2025-07-07 21:05:06 +0100

    Ready for deployment to heroku

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,15 @@
 """Defines URL patterns for users"""
 
 from django.urls import path, include
-
+from django.contrib.auth.views import LogoutView
 from . import views
 
 app_name = 'users'
 urlpatterns = [
-    # Include default auth urls.
+    # Custom logout that accepts GET requests - put this BEFORE the include
+    path('logout/', LogoutView.as_view(), name='logout'),
+    # Include default auth urls
     path('', include('django.contrib.auth.urls')),
-    # Registration page.
+    # Registration page
     path('register/', views.register, name='register'),
- ]
+]
